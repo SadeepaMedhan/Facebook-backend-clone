dda10edd7b3386ebe7be721efd8d8a49b1778d6b
SadeepaMedhan <sadeepamedhan823@gmail.com>
2022-08-23 21:12:35 +0530

    update

--- Models/user.models.js
+++ Models/user.models.js
@@ -10,7 +10,7 @@ const userSchema = new mongoose.Schema({
         required: true
     },
     surename: {
-        type: String,
+        type: String,  
         required: true
     },
     gender: {
--- routes/user.js
+++ routes/user.js
@@ -16,20 +16,63 @@ router.get('/', async(req,res)=>{
 })
 
 router.post('/', (req,res)=>{
-    console.log(req.body);
-    res.send({'message' : 'user not found'})
+    const data = req.body
+    const newUser = new User({
+        id:data.id,
+        first_name: data.first_name,
+        surename: data.surename,
+        gender: data.gender,
+        dob: data.dob,
+        password: data.password,
+        phone: data.phone,
+        email: data.email
+    });
+    try {
+        const response = newUser.save()
+        res.send("Saved!")
+    } catch (error) {
+        res.send(error)
+    }
 })
 
-router.put('/', (req,res)=>{
-    res.send({'message' : 'user not found'})
+router.put('/:id', async(req,res)=>{
+    const data = req.body
+
+    try {
+        const user = await User.findById(req.params.id)
+        user.id = data.id
+        user.first_name = data.first_name
+        user.surename = data.surename
+        user.gender = data.gender
+        user.dob = data.dob
+        user.password = data.password
+        user.phone = data.phone
+        user.email = data.email
+        const response = await user.save()
+
+        res.send("Updated!")
+    } catch (error) {
+        res.send(error)
+    }
 })
 
-router.delete('/:id', (req,res)=>{
-    res.send({'message' : 'user not found'})
+router.delete('/:id', async(req,res)=>{
+    try {
+        const user = await User.findById(req.params.id)
+        const response = user.remove()
+        res.send("Deleted!")
+    } catch (error) {
+        res.send(error)
+    }
 })
 
-router.get('/:id', (req,res)=>{
-    res.send({'message' : 'user not found'})
+router.get('/:id', async(req,res)=>{
+    try {
+        const user = await User.findById(req.params.id)
+        res.send(user)
+    } catch (error) {
+        res.send(error)
+    }
 })
 
 module.exports = router
