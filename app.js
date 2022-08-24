5e236d5b11030711bbff38d4c4a81142823c8eab
SadeepaMedhan <sadeepamedhan823@gmail.com>
2022-08-24 13:14:15 +0530

    add login

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express')
 const mongoose = require('mongoose')
 const user = require('./routes/user')
 const post = require('./routes/post')
+const login = require('./routes/login')
 
 const app = express()
 const port = 4000
@@ -9,6 +10,7 @@ const port = 4000
 app.use(express.json())
 app.use('/user',user)
 app.use('/post',post)
+app.use('/login',login)
 
 const url = 'mongodb://127.0.0.1/fb'
 
